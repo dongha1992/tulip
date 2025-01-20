3bc1611b54a3f051686c98d38883115b814506f1
dongha.kim <dongha.kim@timf.co.kr>
2025-01-20 15:11:25 +0900

    style: sass 추가

--- backend/app.py
+++ backend/app.py
@@ -15,26 +15,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class ImageData(BaseModel):
+    href: str
+    text: str
+    imageSrc: Optional[str]
+
 class CompareImagesRequest(BaseModel):
     target_image: str
-    images: List[str]
+    images: List[ImageData]
 
 @app.post("/compare-images/")
 async def compare_images(request: CompareImagesRequest):
-    similar_images = find_similar_images(request.target_image, request.images)
+    image_urls = [img.imageSrc for img in request.images if img.imageSrc]
+
+    # 이미지 유사도 비교 로직 실행
+    similar_images = find_similar_images(request.target_image, image_urls)
+
     return similar_images
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-#     image_urls = [img.imageSrc for img in request.images if img.imageSrc]
-#
-#     # 이미지 유사도 비교 로직 실행
-#     similar_images = find_similar_images(request.target_image, image_urls)
-#
-#     return similar_images
-#
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
