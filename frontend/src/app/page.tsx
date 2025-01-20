5257915a3c57ff7913991859a2eaa065ac436518
dongha1992 <kamdunga2002@gmail.com>
2025-01-20 15:11:25 +0900

    style: sass 추가

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,21 @@
 'use client';
 
-import styles from './page.module.css';
+import styles from './page.module.scss';
 import { useEffect } from 'react';
 import axios from 'axios';
 
 export default function Home() {
   useEffect(() => {
     const getScraper = async () => {
-      const data = await axios.post('/api/scraper');
-      console.log(data);
+      // const data = await axios.post('/api/scraper');
+      // console.log(data);
     };
 
     getScraper();
   }, []);
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.page}>ass</div>
+    </div>
+  );
 }
