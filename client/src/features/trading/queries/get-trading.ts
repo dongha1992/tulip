252ce148b4536504feb731ba71cf0619b731f568
dongha1992 <kamdunga2002@gmail.com>
2026-01-29 14:25:03 +0900

    fix: update get-trading query

--- client/src/features/trading/queries/get-trading.ts
+++ client/src/features/trading/queries/get-trading.ts
@@ -1,32 +1,33 @@
-import { getAuth } from "@/features/auth/queries/get-auth";
-import { isOwner } from "@/features/auth/utils/is-owner";
-import { prisma } from "@/lib/prisma";
+'use server';
 
+import { getAuth } from '@/features/auth/queries/get-auth';
+import { isOwner } from '@/features/auth/utils/is-owner';
+import { prisma } from '@/lib/prisma';
 
-export const getTrading = async (id: string) => { 
-    const { user } = await getAuth();
-    const trading = await prisma.trading.findUnique({
-        where: {
-            id,
+export const getTrading = async (id: string) => {
+  const { user } = await getAuth();
+  const trading = await prisma.trading.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      user: {
+        select: {
+          username: true,
         },
-          include: {
-        user: {
-            select: {
-            username: true,
-            },
-          },
-        },
-      });
+      },
+    },
+  });
 
   if (!trading) {
     return null;
   }
-    
- return {
+
+  return {
     ...trading,
     isOwner: isOwner(user, trading),
     permissions: {
-      canDeleteTrading: isOwner(user, trading) 
+      canDeleteTrading: isOwner(user, trading),
     },
   };
-}
+};
